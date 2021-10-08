032bec75c3232f0aa203ed4b7671a3000784ab78
Riccardo-Grasso <riccardo.grasso@outlook.it>
2021-10-08 16:14:21 +0200

    finito

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 alert("Benvenuti sul nuovo portale di Ticket Online.");
 
-const chilometri = prompt("Inserire il numero di chilometri (km) che si desidera percorrere. Verrà applicata la tariffa di 0,21€/km.");
+const chilometri = prompt("Inserire il numero di chilometri (km) che si desidera percorrere.\nVerrà applicata la tariffa di 0,21€/km.");
 
 if (isNaN(chilometri)) {
     alert("Valore inserito non valido. Si prega di riprovare.");
@@ -20,16 +20,16 @@ let percentualeScontoOver = 40;
 const scontoOver = ((prezzo / 100) * percentualeScontoOver).toFixed(2);
 const prezzoOver = (prezzo - scontoOver).toFixed(2);
 
-const eta = prompt("Vi informiamo che saranno applicati sconti a minorenni e passeggeri Over-65. Inserite la vostra età per scoprire se potete usufruire dell'offerta!");
+const eta = prompt("Vi informiamo che saranno applicati sconti a minorenni e passeggeri Over-65.\nInserite la vostra età per scoprire se potete usufruire dell'offerta!");
 
 if (isNaN(eta)) {
     alert("Valore inserito non valido. Si prega di riprovare.");
 } else if (eta < 18) {
-    alert(`In quanto passeggero minorenne ha diritto ad uno sconto del ${percentualeScontoMinorenne}% pari a ${scontoMinorenne}€. Il prezzo finale del biglietto scontato è ${prezzoMinorenne}€.`);
+    alert(`In quanto passeggero minorenne ha diritto ad uno sconto del ${percentualeScontoMinorenne}% pari a ${scontoMinorenne}€.\nIl prezzo finale del biglietto scontato è ${prezzoMinorenne}€.`);
     console.log(`Sconto passeggero minorenne: ${scontoMinorenne}€.`);
     console.log(`Prezzo finale: ${prezzoMinorenne}€.`);
 } else if (eta >= 65) {
-    alert(`In quanto passeggero Over-65 ha diritto ad uno sconto del ${percentualeScontoOver}% pari a ${scontoOver}€. Il prezzo finale del biglietto scontato è ${prezzoOver}€.`);
+    alert(`In quanto passeggero Over-65 ha diritto ad uno sconto del ${percentualeScontoOver}% pari a ${scontoOver}€.\nIl prezzo finale del biglietto scontato è ${prezzoOver}€.`);
     console.log(`Sconto passeggero Over-65: ${scontoOver}€.`);
     console.log(`Prezzo finale: ${prezzoOver}€.`);
 } else {
