7964379595167a927daeb17328cfeb6bfaa1ab95
Riccardo-Grasso <riccardo.grasso@outlook.it>
2021-10-11 10:22:40 +0200

    versione_3.0

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,40 @@
 alert("Benvenuti sul nuovo portale di Ticket Online.");
 
-const chilometri = prompt("Inserire il numero di chilometri (km) che si desidera percorrere.\nVerrà applicata la tariffa di 0,21€/km.");
+const chilometri = parseInt(prompt("Inserire il numero di chilometri (km) che si desidera percorrere.\nVerrà applicata la tariffa di 0,21€/km."));
 
 if (isNaN(chilometri)) {
-    alert("Valore inserito non valido. Si prega di riprovare.");
+    alert("Valore chilometri inserito non valido. Si prega di riprovare.");
+
 } else {
-    console.log(`L'utente deve percorrere ${chilometri} km.`);
-}
+    const eta = parseInt(prompt("Vi informiamo che saranno applicati sconti a minorenni e passeggeri Over-65.\nInserite la vostra età per scoprire se potete usufruire dell'offerta!"));
 
-const prezzo = (chilometri * 0.21).toFixed(2);
-console.log(`L'utente pagherà ${prezzo}€`);
-alert(`In base alle tariffe applicate, il biglietto costerà ${prezzo}€.`);
+    if (isNaN(eta)) {
+        alert("Valore età inserito non valido. Si prega di riprovare.");
 
-let percentualeScontoMinorenne = 20;
-const scontoMinorenne = ((prezzo / 100) * percentualeScontoMinorenne).toFixed(2);
-const prezzoMinorenne = (prezzo - scontoMinorenne).toFixed(2);
+    } else {
+        console.log(`L'utente deve percorrere ${chilometri} km.`);
 
-let percentualeScontoOver = 40;
-const scontoOver = ((prezzo / 100) * percentualeScontoOver).toFixed(2);
-const prezzoOver = (prezzo - scontoOver).toFixed(2);
+        const prezzo = (chilometri * 0.21).toFixed(2);
+        console.log(`L'utente pagherà ${prezzo}€`);
 
-const eta = prompt("Vi informiamo che saranno applicati sconti a minorenni e passeggeri Over-65.\nInserite la vostra età per scoprire se potete usufruire dell'offerta!");
+        let percentualeScontoMinorenne = 20;
+        const scontoMinorenne = ((prezzo / 100) * percentualeScontoMinorenne).toFixed(2);
+        const prezzoMinorenne = (prezzo - scontoMinorenne).toFixed(2);
 
-if (isNaN(eta)) {
-    alert("Valore inserito non valido. Si prega di riprovare.");
-} else if (eta < 18) {
-    alert(`In quanto passeggero minorenne ha diritto ad uno sconto del ${percentualeScontoMinorenne}% pari a ${scontoMinorenne}€.\nIl prezzo finale del biglietto scontato è ${prezzoMinorenne}€.`);
-    console.log(`Sconto passeggero minorenne: ${scontoMinorenne}€.`);
-    console.log(`Prezzo finale: ${prezzoMinorenne}€.`);
-} else if (eta > 65) {
-    alert(`In quanto passeggero Over-65 ha diritto ad uno sconto del ${percentualeScontoOver}% pari a ${scontoOver}€.\nIl prezzo finale del biglietto scontato è ${prezzoOver}€.`);
-    console.log(`Sconto passeggero Over-65: ${scontoOver}€.`);
-    console.log(`Prezzo finale: ${prezzoOver}€.`);
-} else {
-    alert("Siamo spiacenti, non rientra in nessuna categoria a cui viene applicato lo sconto.");
+        let percentualeScontoOver = 40;
+        const scontoOver = ((prezzo / 100) * percentualeScontoOver).toFixed(2);
+        const prezzoOver = (prezzo - scontoOver).toFixed(2);
+
+        if (eta < 18) {
+            alert(`In base alle tariffe applicate, il biglietto costerà ${prezzo}€.\nIn quanto passeggero minorenne ha diritto ad uno sconto del ${percentualeScontoMinorenne}% pari a ${scontoMinorenne}€.\nIl prezzo finale del biglietto scontato è ${prezzoMinorenne}€.`);
+            console.log(`Sconto passeggero minorenne: ${scontoMinorenne}€.`);
+            console.log(`Prezzo finale: ${prezzoMinorenne}€.`);
+        } else if (eta > 65) {
+            alert(`In base alle tariffe applicate, il biglietto costerà ${prezzo}€.\nIn quanto passeggero Over-65 ha diritto ad uno sconto del ${percentualeScontoOver}% pari a ${scontoOver}€.\nIl prezzo finale del biglietto scontato è ${prezzoOver}€.`);
+            console.log(`Sconto passeggero Over-65: ${scontoOver}€.`);
+            console.log(`Prezzo finale: ${prezzoOver}€.`);
+        } else {
+            alert(`Siamo spiacenti, non rientra in nessuna categoria a cui viene applicato lo sconto. Il base alle tariffe standard, il suo biglietto costa: ${prezzo}€`);
+        }
+    }
 }
